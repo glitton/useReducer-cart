4ad736af7c7449f53023adaa6eddbaa904ac8691
glitton <glcodeworks@gmail.com>
2023-11-14 13:57:07 -0800

    set up context, reducer, and actions

---
+++ src/reducer.js
@@ -0,0 +1,14 @@
+import {
+  CLEAR_CART,
+  REMOVE,
+  INCREASE,
+  DECREAESE,
+  LOADING,
+  DISPLAY_ITEMS,
+} from "./actions";
+
+const reducer = (state, action) => {
+  return state;
+};
+
+export default reducer;
