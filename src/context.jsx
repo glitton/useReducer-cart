cc796441d21486ee897315615bd3a2240ae04710
glitton <glcodeworks@gmail.com>
2023-11-20 13:40:19 -0800

    increase and decrease functionality

--- src/context.jsx
+++ src/context.jsx
@@ -27,8 +27,16 @@ export const AppProvider = ({ children }) => {
     dispatch({ type: REMOVE, payload: { id } });
   };
 
+  const increaseItem = (id) => {
+    dispatch({ type: INCREASE, payload: { id } });
+  };
+  const decreaseItem = (id) => {
+    dispatch({ type: DECREASE, payload: { id } });
+  };
   return (
-    <AppContext.Provider value={{ ...state, clearCart, removeItem }}>
+    <AppContext.Provider
+      value={{ ...state, clearCart, removeItem, increaseItem, decreaseItem }}
+    >
       {children}
     </AppContext.Provider>
   );
